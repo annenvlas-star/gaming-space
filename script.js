8b75587bed9cbf08473e91236fa357974436d73b
annenvlas-star <annenvlas@gmail.com>
2026-06-26 16:55:50 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+const lesson = {
+  title: "At School",
+  words: ["book", "pen", "ruler"]
+};
+
+let currentStep = 0;
+
+function startLesson() {
+  document.getElementById("start-screen").classList.add("hidden");
+  document.getElementById("lesson-screen").classList.remove("hidden");
+
+  document.getElementById("lesson-title").innerText = lesson.title;
+
+  renderWords();
+}
+
+function renderWords() {
+  const container = document.getElementById("words");
+  container.innerHTML = "";
+
+  lesson.words.forEach(word => {
+    const div = document.createElement("div");
+    div.className = "word";
+    div.innerText = word;
+    container.appendChild(div);
+  });
+}
+
+function nextStep() {
+  currentStep++;
+
+  alert("Next stage coming soon 🚀 (we will build games here)");
+}
 
